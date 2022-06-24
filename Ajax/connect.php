e3a24dad386256c2f2ef685a24fc0f9dc178af18
pguiderdoni <101812669+pguiderdoni@users.noreply.github.com>
2022-06-24 10:37:43 +0200

    24/06

--- Ajax/connect.php
+++ Ajax/connect.php
@@ -1,26 +1,25 @@
 <?php session_start();
 ?> 
 <?php
-
+$log = 'log.txt';
 switch($_POST['request'])
 {
  case 'connexion':
 
 	$status = 1;
 	$msg = "Connexion OK!";
-	$log = 'log.txt';
 
 	if($_POST['login'] != "pierre"){
 		$status = 0;
 		$msg = "Login incorrect";
 		file_put_contents($log, 'ID: '.$_POST['login'].' '.'le'.date(" d-m-Y H:i:s")." Connexion échouée: Utilisateur inconnu. \n", FILE_APPEND);
 	}
-	if($_POST['password'] !== "ppp"){
+	else if($_POST['password'] !== "ppp"){
 		$status = 0;
 		$msg = "Mot de passe incorrect";
 		file_put_contents($log, 'ID: '.$_POST['login'].' '.'le'.date(" d-m-Y H:i:s")." Connexion échouée: Mot de passe incorrect. \n", FILE_APPEND);
 	}
-	if ($status == 1){
+	else if ($status == 1){
 		$user = $_POST['login'];
 		$_SESSION['loggedUser'] = $user;
 		file_put_contents($log, 'ID: '.$_POST['login'].' '.'le'.date(" d-m-Y H:i:s")." Connexion réussie \n", FILE_APPEND);
@@ -46,8 +45,8 @@ switch($_POST['request'])
   case 'logout':
 	$id = $_SESSION['loggedUser'];
 	$status = 0;
-	session_destroy();
-	// file_put_contents($log, 'ID: '.$id.' '.'le'.date(" d-m-Y H:i:s")." Déconnexion \n", FILE_APPEND);
+	session_destroy();	
+	file_put_contents($log, 'ID: '.$id.' '.'le'.date(" d-m-Y H:i:s")." Déconnexion \n", FILE_APPEND);
 	echo json_encode($status);
   break;
 	
